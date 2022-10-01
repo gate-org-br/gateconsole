7c99f9dfcf8b59a88643a519469ce445c5734986
davins <davinunesdasilva@gmail.com>
2022-10-01 18:35:51 -0300

    Implement _search link target

--- src/main/java/gateconsole/modules/admin/DemoScreen.java
+++ src/main/java/gateconsole/modules/admin/DemoScreen.java
@@ -7,11 +7,15 @@ import gate.annotation.Handler;
 import gate.annotation.Icon;
 import gate.annotation.Name;
 import gate.annotation.Public;
+import gate.entity.User;
+import gate.error.AppException;
 import gate.error.ConversionException;
 import gate.handler.JsonTextHandler;
+import gate.handler.OptionHandler;
 import gate.io.URL;
 import gate.report.Doc;
 import gate.report.Report;
+import gate.type.ID;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -29,6 +33,22 @@ public class DemoScreen extends gate.base.Screen
 	private String text;
 	private Doc.Type type;
 	private List<Doc.Type> types = new ArrayList<>();
+
+	private static final List<User> USERS
+		= List.of(new User().setId(new ID(1)).setName("Amanda Rodrigues").setUsername("amandarodrigues").setUsername("amandarodrigues@gate.org"),
+			new User().setId(new ID(2)).setName("Amanda Borges").setUsername("amandaborges").setUsername("amandaborges@gate.org"),
+			new User().setId(new ID(2)).setName("Anderson Borges").setUsername("andersonborges").setUsername("andersonborges@gate.org"),
+			new User().setId(new ID(2)).setName("Anderson Lima").setUsername("andersonlima").setUsername("andersonlima@gate.org"),
+			new User().setId(new ID(3)).setName("Barbara Gomes").setUsername("barbaragomes").setUsername("bgomes@gate.org"),
+			new User().setId(new ID(4)).setName("Barbie Rodrigues").setUsername("barbierodrigues").setUsername("brodrigues@gate.org"),
+			new User().setId(new ID(5)).setName("Barbie Sampaio").setUsername("barbiesampaio").setUsername("barbiesampaio@gate.org"),
+			new User().setId(new ID(6)).setName("Bartira Souza").setUsername("bartirasouza").setUsername("bartirasouza@gate.org"),
+			new User().setId(new ID(7)).setName("Bartira Nunes").setUsername("bartiranunes").setUsername("bartiranunes@gate.org"),
+			new User().setId(new ID(8)).setName("Bartolomeu Borges").setUsername("bartolomeuborges").setUsername("bartolomeuborges@gate.org"),
+			new User().setId(new ID(9)).setName("Bruno Silva").setUsername("brunosilva").setUsername("brunosilva@gate.org"),
+			new User().setId(new ID(10)).setName("Carla Sampaio").setUsername("carlasampaio").setUsername("carlasampaio@gate.org"),
+			new User().setId(new ID(11)).setName("Carlos Gomes").setUsername("carlosgomes").setUsername("carlosgomes@gate.org"),
+			new User().setId(new ID(12)).setName("Carlene Souza").setUsername("carlenesouza").setUsername("carlenesouza@gate.org"));
 
 	public String call()
 	{
@@ -119,7 +139,7 @@ public class DemoScreen extends gate.base.Screen
 			try
 			{
 				Progress.update("Processing " + i);
-				Thread.sleep(50);;
+				Thread.sleep(50);
 			} catch (InterruptedException ex)
 			{
 				Progress.cancel(ex.getMessage());
@@ -152,6 +172,21 @@ public class DemoScreen extends gate.base.Screen
 
 		int size = getRequest().getParameter(Integer.class, "size");
 		return TestData.INSTANCE.stream().skip(size).limit(10).collect(Collectors.toList());
+	}
+
+	@Handler(OptionHandler.class)
+	public List<User> callSelectOptions()
+	{
+		return USERS;
+	}
+
+	@Handler(OptionHandler.class)
+	public List<User> callSearchOptions() throws AppException
+	{
+		String body = getRequest().getBody().trim().toUpperCase();
+		return body.length() >= 3
+			? USERS.stream().filter(e -> e.getName().toUpperCase().contains(body)).collect(Collectors.toList())
+			: List.of();
 	}
 
 	public void setType(Doc.Type type)
